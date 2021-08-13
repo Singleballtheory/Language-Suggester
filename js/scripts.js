9ebd27debfc0ad2694d11e30375343921da8d9ee
Scott Hutley <scotthutley1@comcast.net>
2021-08-13 12:43:11 -0700

    WIP surveyTotal workinggit add .! Code still incomplete

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,13 @@ $(document).ready(function() {
     const val3 = parseInt($("#question3").val());
     const val4 = parseInt($("#question4").val());
     const val5 = parseInt($("#question5").val());
-    result = surveyTotal(val1, val2, val3, val4, val5);
-    
+    let total = surveyTotal(val1, val2, val3, val4, val5);
+    alert (total);
+
+    if (total <= 8) {
+      alert ("less than 8");
+    } else if (total > 8 && total <= 12) {
+      alert ("between 9 and 12");
+    } else alert ("not ready yet");
   });
 });
